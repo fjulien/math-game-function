ab484711e976b7dc210a8dc00023c38bcb50ceb3
fjulien <fettingerjulien@yahoo.fr>
2020-04-19 15:41:56 +0200

    Add transpilation into ecmaScript5

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-export const MIN = 0;
-export const MAX = 9;
-export const ERROR_PARAMS = "params missing";
-export const ERROR_TYPE_PARAMS = "params is not a number";
-export const ERROR_SIZE_PARAMS = "params is not between 0 and 9";
+"use strict";
 
-export function makeRandomNumber() {return Math.floor((Math.random() * 10))};
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.makeRandomNumber = makeRandomNumber;
+exports.multiply = multiply;
+exports.ERROR_SIZE_PARAMS = exports.ERROR_TYPE_PARAMS = exports.ERROR_PARAMS = exports.MAX = exports.MIN = void 0;
+var MIN = 0;
+exports.MIN = MIN;
+var MAX = 9;
+exports.MAX = MAX;
+var ERROR_PARAMS = "params missing";
+exports.ERROR_PARAMS = ERROR_PARAMS;
+var ERROR_TYPE_PARAMS = "params is not a number";
+exports.ERROR_TYPE_PARAMS = ERROR_TYPE_PARAMS;
+var ERROR_SIZE_PARAMS = "params is not between 0 and 9";
+exports.ERROR_SIZE_PARAMS = ERROR_SIZE_PARAMS;
 
+function makeRandomNumber() {
+  return Math.floor(Math.random() * 10);
+}
 
-export function multiply(nb1, nb2){
-    if(nb1 === undefined || nb2 === undefined) throw ERROR_PARAMS;
-    if(typeof nb1 !== "number" || typeof nb2 !== "number") throw ERROR_TYPE_PARAMS;
-    if((nb1 < MIN || nb1 > MAX) || (nb2 < MIN || nb2 > MAX)) throw ERROR_SIZE_PARAMS;
-    return nb1*nb2;
-};
+;
+
+function multiply(nb1, nb2) {
+  if (nb1 === undefined || nb2 === undefined) throw ERROR_PARAMS;
+  if (typeof nb1 !== "number" || typeof nb2 !== "number") throw ERROR_TYPE_PARAMS;
+  if (nb1 < MIN || nb1 > MAX || nb2 < MIN || nb2 > MAX) throw ERROR_SIZE_PARAMS;
+  return nb1 * nb2;
+}
+
+;
